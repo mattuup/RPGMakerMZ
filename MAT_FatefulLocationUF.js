fa88bd75f8e2f1048aec626bf7561bb2c6263efa
mattuup <mattuup@gmail.com>
2020-08-20 20:38:42 +0900

    add

---
+++ MAT_FatefulLocationUF.js
@@ -0,0 +1,74 @@
+//=============================================================================
+// MAT_FatefulLocationUF.js
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020 mattuup
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+//=============================================================================
+
+/*:ja
+ * @plugindesc ver1.00 場所移動時の自動演奏の制御等
+ * @author mattuup
+ * @target MZ
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @url https://github.com/mattuup/RPGMakerMZ
+ * 
+ * @param bgmSwitch
+ * @desc オンにしていると場所移動時設定したBGMを自動演奏しません。
+ * @type switch
+ * @default 10
+ * 
+ * @param bgsSwitch
+ * @desc オンにしていると場所移動時設定したBGSを自動演奏しません。
+ * @type switch
+ * @default 11
+ * 
+ * @param ufinitVariable
+ * @desc このIDの変数は自動演奏タイミングで0になります。
+ * (自動演奏が有効でも無効でも同様です。)
+ * @type variable
+ * @default 0
+ *
+ * @help
+ * 
+ * RPGで笑顔を・・・
+ * 
+ * このヘルプとパラメータの説明をよくお読みになってからお使いください。
+ * 
+ * 利用規約はMITライセンスの通り。
+ * （FatefulLocationUF.js再録）
+ * 
+ * パラメータ設定のスイッチをオンにしている時は場所移動時に
+ * BGMやBGSがマップの自動演奏設定のものにならないため
+ * イベントなどに役立ててください。
+ * （この場所移動はプロジェクト更新後の既存セーブのロード時も含みます。）
+ * 
+ * 
+ */
+
+var Imported = Imported || {};
+Imported.MAT_FatefulLocationUF = true;
+
+(() => {
+
+'use strict';
+
+const script = document.currentScript;
+const param  = PluginManagerEx.createParameter(script);
+
+
+Game_Map.prototype.autoplay = function() {
+    if ($dataMap.autoplayBgm && (param.bgmSwitch <= 0 || !$gameSwitches.value(param.bgmSwitch))) {
+        AudioManager.playBgm($dataMap.bgm);
+    }
+    if ($dataMap.autoplayBgs && (param.bgsSwitch <= 0 || !$gameSwitches.value(param.bgsSwitch))) {
+        AudioManager.playBgs($dataMap.bgs);
+    }
+    if(param.ufinitVariable > 0){
+        $gameVariables.setValue(param.ufinitVariable, 0);
+    }
+};
+
+
+})();
---
+++ MAT_NeedEquiporExpchagemhp.js
@@ -0,0 +1,109 @@
+//=============================================================================
+// MAT_NeedEquiporExpchagemhp.js
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020 mattuup
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+//=============================================================================
+
+/*:ja
+ * @plugindesc ver1.00 装備変更時とレベル変動時のMHP増加の補完
+ * @author mattuup
+ * @target MZ
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @url https://github.com/mattuup/RPGMakerMZ
+ * 
+ * @param equipValid
+ * @desc 装備の当該機能を有効にします。
+ * @default true
+ * @type boolean
+ * 
+ * @param expValid
+ * @desc 経験値の当該機能を有効にします。
+ * @default true
+ * @type boolean
+ * 
+ * @param recoverAllValid
+ * @desc 経験値の当該機能で増加分回復する時、
+ * 代わりに全回復と同様の処理をします。
+ * @default true
+ * @type boolean
+ *
+ * @help
+ * 
+ * RPGで笑顔を・・・
+ * 
+ * このヘルプとパラメータの説明をよくお読みになってからお使いください。
+ * 
+ * 利用規約はMITライセンスの通り。
+ * （NeedEquipchagemhp.jsとNeedexpchangemhp.jsを再録）
+ * 
+ * 装備の変更によって
+ * mhpまたはmmpが増加した場合増加分回復します。
+ * 
+ * レベル変動時（経験値変動時）に
+ * mhpまたはmmpが増加した場合増加分回復します。
+ * 
+ * 各回復にはアクターのリフレッシュが含まれるため
+ * それに伴い、装備の変更や経験値の変更などを行う場合には
+ * 無限回帰を引き落とす可能性があります。
+ * デフォルトではまず問題ありませんが
+ * 他のプラグインと同時に導入する場合はお気を付けください。
+ * 
+ * 
+ */
+
+var Imported = Imported || {};
+Imported.MAT_NeedEquiporExpchagemhp = true;
+
+(() => {
+
+'use strict';
+
+const script = document.currentScript;
+const param  = PluginManagerEx.createParameter(script);
+
+
+Game_Actor.prototype.NEEprearray = function() {
+    return [this.mhp, this.mmp];
+};
+
+Game_Actor.prototype.NEEneedrecover = function(array, recoverAll) {
+    const difhp = this.mhp - array[0];
+    const difmp = this.mmp - array[1];
+    const hpc = (difhp > 0);
+    const mpc = (difmp > 0);
+    if(recoverAll && (hpc || mpc)){
+        this.recoverAll();
+        return;
+    }
+    if(hpc) this.setHp(this.hp + difhp);
+    if(mpc) this.setMp(this.mp + difmp);
+};
+
+if(param.equipValid){
+
+    const _Game_Actor_changeEquip = Game_Actor.prototype.changeEquip;
+    Game_Actor.prototype.changeEquip = function(slotId, item) {
+        const array = this.NEEprearray();
+        _Game_Actor_changeEquip.call(this, slotId, item);
+        this.NEEneedrecover(array, false);
+    };
+
+}
+
+if(param.expValid){
+
+    //レベルダウン時にもmhp等は増える可能性があるためlevelup側ではなくこちらで計算する。
+    const _Game_Actor_changeExp = Game_Actor.prototype.changeExp;
+    Game_Actor.prototype.changeExp = function(exp, show) {
+        const array = this.NEEprearray();
+        _Game_Actor_changeExp.call(this, exp, show);
+        this.NEEneedrecover(array, param.recoverAllValid);
+    };
+
+}
+
+
+})();
